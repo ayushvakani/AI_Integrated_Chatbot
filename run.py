f0f2a35795e42f2b958a9b699a4c38a47e9f0973
ayushvakani <ayush.vakani@gmail.com>
2025-07-08 11:12:06 +0530

    fix: updated run.py to support Render deployment

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 from app.routes import api  
+import os
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(api)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
